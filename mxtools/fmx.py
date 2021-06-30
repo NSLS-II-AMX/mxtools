081eb11c342f72b1ddc2f59b909b30c61f74d591
Jun Aishima <jaishima@bnl.gov>
2021-06-30 11:23:40 -0400

    fix name of vector object, fix relative imports in fmx

--- mxtools/fmx.py
+++ mxtools/fmx.py
@@ -1,5 +1,5 @@
-import vector_program
-import zebra
+from . import vector_program
+from . import zebra
 
 zebra = zebra.Zebra('XF:17IDC-ES:FMX{Zeb:3}:', name='zebra')
-vector_program = vector_program.VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
+vector = vector_program.VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
