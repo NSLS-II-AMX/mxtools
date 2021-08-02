78e919d8482ead121b0e77a8abe9b1d39f9d14e8
Jun Aishima <jaishima@bnl.gov>
2021-08-02 10:17:25 -0400

    fix flake8 issue - line too long

--- mxtools/scans.py
+++ mxtools/scans.py
@@ -159,6 +159,7 @@ def setup_eiger_stop_acquire_and_wait(eiger):
 
 
 # use it as follows:
-# RE(setup_eiger_arming(eiger_single, 0, 100, 10, 0.01, 'test20210729', '/GPFS/CENTRAL/xf17id2/mfuchs/fmxoperator/20200222/mx999999-1665/', 2851, 2002.125, 2245.850, 0.9793, 0.250))
+# RE(setup_eiger_arming(eiger_single, 0, 100, 10, 0.01, 'test20210729',\
+#    '/GPFS/CENTRAL/xf17id2/mfuchs/fmxoperator/20200222/mx999999-1665/', 2851, 2002.125, 2245.850, 0.9793, 0.250))
 # RE(actual_scan(mx_flyer, eiger_single, vector, zebra, 0, 100, 0.2, 0.01))
 # and it should all work - but doesn't at the moment (Eiger not getting triggered)
