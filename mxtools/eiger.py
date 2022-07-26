4edee61897ca12f35b96f6247a5aeb6a1b83c99e
Jun Aishima <jaishima@bnl.gov>
2022-07-26 17:14:38 -0400

    linting again with correct line length

--- mxtools/eiger.py
+++ mxtools/eiger.py
@@ -7,12 +7,10 @@ from types import SimpleNamespace
 
 import h5py
 from ophyd import Component as Cpt
-from ophyd import (Device, EpicsPathSignal, EpicsSignal, ImagePlugin, Signal,
-                   SingleTrigger)
+from ophyd import Device, EpicsPathSignal, EpicsSignal, ImagePlugin, Signal, SingleTrigger
 from ophyd.areadetector import EigerDetector
 from ophyd.areadetector.base import ADComponent, EpicsSignalWithRBV
 from ophyd.areadetector.filestore_mixins import FileStoreBase  # , new_short_uid
-    FileStoreBase  # , new_short_uid
 from ophyd.utils import set_and_wait
 
 from . import print_now
