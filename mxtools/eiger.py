55c351010f0f513a6be730c71015ede36e1d9d50
Jun Aishima <jaishima@bnl.gov>
2022-12-22 19:29:05 -0500

    linting: isort formatting fix

--- mxtools/eiger.py
+++ mxtools/eiger.py
@@ -8,10 +8,12 @@ from types import SimpleNamespace
 
 import h5py
 from ophyd import Component as Cpt
-from ophyd import Device, EpicsPathSignal, EpicsSignal, ImagePlugin, Signal, SingleTrigger
+from ophyd import (Device, EpicsPathSignal, EpicsSignal, ImagePlugin, Signal,
+                   SingleTrigger)
 from ophyd.areadetector import EigerDetector
 from ophyd.areadetector.base import ADComponent, EpicsSignalWithRBV
-from ophyd.areadetector.filestore_mixins import FileStoreBase  # , new_short_uid
+from ophyd.areadetector.filestore_mixins import \
+    FileStoreBase  # , new_short_uid
 
 from . import print_now
 
