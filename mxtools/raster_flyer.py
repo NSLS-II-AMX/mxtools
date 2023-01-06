e3f6bdc4ddfbc792c27e5eb8ff7bb9d75d84cab3
Jun Aishima <jaishima@bnl.gov>
2023-01-06 16:07:38 -0500

    add wait=True for zebra PVs to prevent rastering issues
     * the pc.pulse.delay in particular seems to be the PV whose
       value may lag for 1 second+ as monitored with camonitor,
       resulting in behavior as if it is with the old value
     * use_complete, a kwarg in Ophyd, was attempted but did
       not seem to work - wait is a PyEpics PV object kwarg

--- mxtools/raster_flyer.py
+++ mxtools/raster_flyer.py
@@ -87,16 +87,20 @@ class MXRasterFlyer(MXFlyer):
         num_images,
         is_still=False,
     ):
-        self.zebra.pc.gate.start.put(angle_start)
+        self.zebra.pc.gate.start.put(angle_start, wait=True)
         if is_still is False:
-            self.zebra.pc.gate.width.put(gate_width)
-            self.zebra.pc.gate.step.put(scan_width)
-        self.zebra.pc.gate.num_gates.put(1)
-        self.zebra.pc.pulse.start.put(0)
-        self.zebra.pc.pulse.width.put(pulse_width)
-        self.zebra.pc.pulse.step.put(pulse_step)
-        self.zebra.pc.pulse.delay.put(exposure_period_per_image / 2 * 1000)
-        self.zebra.pc.pulse.max.put(num_images)
+            logger.debug(f"gate width: {gate_width} gate step: {scan_width}")
+            self.zebra.pc.gate.width.put(gate_width, wait=True)
+            self.zebra.pc.gate.step.put(scan_width, wait=True)
+        self.zebra.pc.gate.num_gates.put(1, wait=True)
+        self.zebra.pc.pulse.start.put(0, wait=True)
+        logger.debug(f"pulse width: {pulse_width}")
+        self.zebra.pc.pulse.width.put(pulse_width, wait=True)
+        self.zebra.pc.pulse.step.put(pulse_step, wait=True)
+        logger.debug(f"pulse delay: {exposure_period_per_image / 2 * 1000}")
+        self.zebra.pc.pulse.delay.put(exposure_period_per_image / 2 * 1000, wait=True)
+        logger.debug(f"after setting: gate width: {self.zebra.pc.gate.width.get()} gate step: {self.zebra.pc.gate.step.get()} pulse width: {self.zebra.pc.pulse.width.get()} pulse delay: {self.zebra.pc.pulse.delay.get()}")
+        self.zebra.pc.pulse.max.put(num_images, wait=True)
         self.vector.hold.put(0)  # necessary to prevent problems upon
         # exposure time change
 
